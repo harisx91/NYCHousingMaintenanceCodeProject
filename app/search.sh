4624b7916ebd2ddc705abc8cfcb6eafa302ece62
Benny c <cohenb50@gmail.com>
2019-12-14 23:03:44 -0500

    added building analysis, incident analysis

---
+++ app/search.sh
@@ -0,0 +1,17 @@
+touch results
+> results
+wild=*
+while [ 1=1 ]
+do
+Echo 'Enter a search term or enter quit to see results and leave'
+read searchTerm
+if [ $searchTerm = 'quit' ]; then
+  break
+fi
+echo $searchTerm >> results
+egrep -c -w -i *$searchTerm* novdescriptions.csv >> results
+echo "results recorded"
+echo "results recorded"
+done
+
+py CreateGraph.py
